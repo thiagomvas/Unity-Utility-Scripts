922bef63171b66346bb629dca9bb1410d2ca075a
Gaxihyus <47324415+Gaxihyus@users.noreply.github.com>
2023-02-06 13:36:04 -0300

    Update SerializationManager.cs

--- SaveSystem/SerializationManager.cs
+++ SaveSystem/SerializationManager.cs
@@ -2,37 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
-using UnityEngine.Events;
 
 public static class SerializationManager
 {
-    public static UnityEvent OnSave = new UnityEvent();
+    static string dir = Application.persistentDataPath + "/saves/"; // Directory to save in
     
-
-
-    static string dir = Application.persistentDataPath + "/saves/";
-    public static bool SaveGame(string saveName, object saveData)
+    //The saveData is given as SaveData.Current
+    public static bool SaveGame(string saveName, object saveData) 
     {
         if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
 
 
-        string json = JsonUtility.ToJson(saveData, true);
+        string json = JsonUtility.ToJson(saveData, true); // Converts data saved to json
 
-        File.WriteAllText(dir + saveName + ".save", json);
+        File.WriteAllText(dir + saveName + ".save", json); // Saves to disk
 
         return true;
     }
 
     public static bool LoadGame(string saveName)
     {
-        if(!File.Exists(dir + saveName)) return false;
+        if(!File.Exists(dir + saveName)) return false; // Failed Load Check
 
         SaveData temp = new SaveData();
 
-        string data = File.ReadAllText(dir + saveName);
-        temp = JsonUtility.FromJson<SaveData>(data);
+        string data = File.ReadAllText(dir + saveName); // Reading the json file
+        temp = JsonUtility.FromJson<SaveData>(data); // Converting from json to SaveData
 
-        SaveData.Current = temp;
+        SaveData.Current = temp; // Updating Data
 
         return true;
     }
